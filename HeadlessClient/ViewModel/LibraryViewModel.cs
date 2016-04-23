c37b4dd1a09132911591c7a5e3f008745e0d38a0
Luke Wilson <heylukewilson@gmail.com>
2016-04-22 17:37:56 -0700

    initial commit

---
+++ HeadlessClient/ViewModel/LibraryViewModel.cs
@@ -0,0 +1,38 @@
+﻿using GalaSoft.MvvmLight;
+using System.Collections.Generic;
+
+namespace HeadlessClient.ViewModel
+{
+    public class LibraryViewModel : ViewModelBase
+    {
+        public LibraryViewModel()
+        {
+            List<string> test = new List<string>();
+            test.Add("Link 1");
+            test.Add("Link 2");
+            test.Add("Link 3");
+            Experiences = test;
+        }
+
+        public const string ExperiencesPropertyName = "Experiences";
+        private List<string> _experiences;
+        public List<string> Experiences
+        {
+            get
+            {
+                return _experiences;
+            }
+
+            set
+            {
+                if (_experiences == value)
+                {
+                    return;
+                }
+
+                _experiences = value;
+                RaisePropertyChanged(ExperiencesPropertyName);
+            }
+        }
+    }
+}
---
+++ HeadlessClient/Views/LibraryView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace HeadlessClient.Views
+{
+    /// <summary>
+    /// Description for LibraryView.
+    /// </summary>
+    public partial class LibraryView
+    {
+        /// <summary>
+        /// Initializes a new instance of the LibraryView class.
+        /// </summary>
+        public LibraryView()
+        {
+            InitializeComponent();
+        }
+    }
+}
